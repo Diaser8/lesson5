1e969ea969297f4b65449ceec0c563349cb6fcc0
Diaser8 <diaser1987@gmail.com>
2024-05-11 19:17:40 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+immutable_var = ( 1, 2, True, 'work' )
+print (immutable_var)
+immutable_var [0] = 5
+print( immutable_var )
+mutable_list = [0, 1, 2, 'uraboros', False]
+print( mutable_list )
+mutable_list[1] = 8
+print( mutable_list )
